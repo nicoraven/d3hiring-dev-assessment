91a7284fd8a1d5df458d5bf0038df601f937cbb9
nicoraven <nicoraventbh@gmail.com>
2019-10-16 10:34:20 +0800

    updated settings

--- scripts/createDB.js
+++ scripts/createDB.js
@@ -6,12 +6,15 @@ const dbName = process.env.DB_SCHEMAS || 'SAS';
 
 mysql.createConnection({
   host: process.env.DB_HOST || '127.0.0.1',
-  port: process.env.DB_PORT || '3006',
+  port: process.env.DB_PORT || '3306',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || 'password',
 }).then((connection) => {
   connection.query(`CREATE DATABASE IF NOT EXISTS ${dbName};`).then((res) => {
     console.info('Database created or successfully checked');
     process.exit(0);
-  });
+  })
+    .catch((error) => {
+      console.log(error);
+    });
 });
